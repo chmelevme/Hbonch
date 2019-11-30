85087fa1e402e057eb1e31bafc2993971e01731b
Mikhail <chmelev.mikhail@gmail.com>
2019-11-30 16:47:49 +0300

    feat: created database

---
+++ tests.py
@@ -0,0 +1,91 @@
+import unittest
+from webapp import create_app, db
+from config import config
+from webapp.models import User, Deadline, Level, Group, Deadline_status
+from datetime import datetime, timedelta
+
+
+class Testingconfig(config):
+    TESTING = True
+    SQLALCHEMY_DATABASE_URI = 'sqlite://'
+    ELASTICSEARCH_URL = None
+
+
+class tests(unittest.TestCase):
+    def setUp(self) -> None:
+        self.app = create_app(Testingconfig)
+        self.app_context = self.app.app_context()
+        self.app_context.push()
+        db.create_all()
+
+    def tearDown(self) -> None:
+        db.session.remove()
+        db.drop_all()
+        self.app_context.pop()
+
+    def test_deadline_value(self):
+        now = datetime.utcnow()
+        now2 = now + timedelta(minutes=10)
+        d = Deadline(expiration_date=now, title='first')
+        d2 = Deadline(expiration_date=now2, title='second')
+        level = Level(value=10)
+        db.session.add_all([d, d2, level])
+        db.session.commit()
+        level.deadlines.append(d)
+        level.deadlines.append(d2)
+        db.session.commit()
+        self.assertEqual('[<Deadline 1>, <Deadline 2>]', str(level.deadlines.all()))
+        self.assertEqual(10, d.level.value)
+        self.assertEqual(10, d2.level.value)
+        self.assertNotEqual(1100, d2.level.value)
+
+    def test_deadline_group(self):
+        now = datetime.utcnow()
+        now2 = now + timedelta(minutes=10)
+        d = Deadline(expiration_date=now, title='first')
+        d2 = Deadline(expiration_date=now2, title='second')
+        group = Group(name='Test_group')
+        group.deadlines.append(d)
+        group.deadlines.append(d2)
+        db.session.add_all([d, d2, group])
+        db.session.commit()
+        self.assertEqual(d.group.name, 'Test_group')
+        self.assertNotEqual(d.group.name, 'Test_group2')
+        self.assertEqual(d2.group.name, 'Test_group')
+
+    def test_user_deadline_status(self):
+        now = datetime.utcnow()
+        now2 = now + timedelta(minutes=10)
+        d = Deadline(expiration_date=now, title='first')
+        d2 = Deadline(expiration_date=now2, title='second')
+        level = Level(value=10)
+        level.deadlines.append(d)
+        level.deadlines.append(d2)
+        u = User(name='Test_user', email='Test_user_email')
+        u.set_password('123')
+        db.session.add_all([d, level, u])
+        db.session.commit()
+        d_s = Deadline_status(user_id=u.id, deadline_id=d.id)
+        d_s2 = Deadline_status(user_id=u.id, deadline_id=d2.id)
+        db.session.add_all([d_s, d_s2])
+        d_s2.status = 3
+        db.session.commit()
+        self.assertEqual(2, Deadline_status.query.filter_by(user_id=u.id, deadline_id=d.id).first().status)
+        self.assertEqual(3, Deadline_status.query.filter_by(user_id=u.id, deadline_id=d2.id).first().status)
+        self.assertNotEqual(2, Deadline_status.query.filter_by(user_id=u.id, deadline_id=d2.id).first().status)
+
+    def test_self_group(self):
+        u = User(name='Test_user', email='Test_user_email')
+        group = Group(name='Test_group')
+        now = datetime.utcnow()
+        d = Deadline(expiration_date=now, title='first')
+        group.deadlines.append(d)
+        u.self_group = group
+        level = Level(value=10)
+        level.deadlines.append(d)
+        self.assertEqual('Test_group', u.self_group.name)
+        self.assertEqual(10, u.self_group.deadlines.first().level.value)
+        self.assertNotEqual(120, u.self_group.deadlines.first().level.value)
+
+if __name__ == '__main__':
+    unittest.main(verbosity=2)
--- webapp/models.py
+++ webapp/models.py
@@ -9,10 +9,17 @@ def load_user(id):
     return User.query.get(int(id))
 
 
-members = db.table('members',
-                   db.Column('user_id', db.Integer, db.ForeignKey('User.id'), primary_key=True),
-                   db.Column('group_id', db.Integer, db.ForeignKey('Group.id'), primary_key=True),
-                   db.Column('points', db.Integer))
+members = db.Table('members',
+                   db.Column('user_id', db.Integer, db.ForeignKey('user.id'), primary_key=True),
+                   db.Column('group_id', db.Integer, db.ForeignKey('group.id'), primary_key=True),
+                   db.Column('points', db.Integer, default=0)
+                   )
+
+
+class Deadline_status(db.Model):
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), primary_key=True)
+    deadline_id = db.Column(db.Integer, db.ForeignKey('deadline.id'), primary_key=True)
+    status = db.Column(db.Integer, nullable=False, default=2)
 
 
 class User(UserMixin, db.Model):
@@ -20,6 +27,10 @@ class User(UserMixin, db.Model):
     name = db.Column(db.String(20), unique=True)
     password = db.Column(db.String(20))
     email = db.Column(db.String(30), unique=True)
+    deadline_statuses = db.relationship('Deadline_status', lazy='joined')
+    groups = db.relationship('Group', secondary=members, lazy='subquery',
+                             backref=db.backref('members', lazy='subquery'))
+    self_group = db.relationship('Group', secondary=members, uselist=False)
 
     def set_password(self, password):
         self.password = generate_password_hash(password)
@@ -31,15 +42,19 @@ class User(UserMixin, db.Model):
 class Group(db.Model):
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     name = db.Column(db.String(20), nullable=False)
+    deadlines = db.relationship('Deadline', backref=db.backref('group'), lazy='dynamic')
 
 
 class Deadline(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     expiration_date = db.Column(db.DateTime, nullable=False)
     title = db.Column(db.String, nullable=False)
-    value_id = db.Column(db.Integer, db.ForeignKey('level.id'), nullable=False)
+    level_id = db.Column(db.Integer, db.ForeignKey('level.id'))
+    group_id = db.Column(db.Integer, db.ForeignKey('group.id'))
+    users_statuses = db.relationship('Deadline_status', lazy='joined')
 
 
 class Level(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     value = db.Column(db.Integer, nullable=False)
+    deadlines = db.relationship('Deadline', backref='level', lazy='dynamic')
