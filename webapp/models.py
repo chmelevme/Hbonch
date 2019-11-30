1457663a799e89f59052daa5dbb6233fc9fad2cc
Mikhail <chmelev.mikhail@gmail.com>
2019-11-30 14:49:07 +0300

    feat: do testing

--- webapp/models.py
+++ webapp/models.py
@@ -9,6 +9,12 @@ def load_user(id):
     return User.query.get(int(id))
 
 
+members = db.table('members',
+                   db.Column('user_id', db.Integer, db.ForeignKey('User.id'), primary_key=True),
+                   db.Column('group_id', db.Integer, db.ForeignKey('Group.id'), primary_key=True),
+                   db.Column('points', db.Integer))
+
+
 class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     name = db.Column(db.String(20), unique=True)
@@ -20,3 +26,20 @@ class User(UserMixin, db.Model):
 
     def check_password(self, password):
         return check_password_hash(self.password, password)
+
+
+class Group(db.Model):
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    name = db.Column(db.String(20), nullable=False)
+
+
+class Deadline(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    expiration_date = db.Column(db.DateTime, nullable=False)
+    title = db.Column(db.String, nullable=False)
+    value_id = db.Column(db.Integer, db.ForeignKey('level.id'), nullable=False)
+
+
+class Level(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    value = db.Column(db.Integer, nullable=False)
