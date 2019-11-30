a8c7505df40eced68923c9aed2894cbafe8bad4e
Mikhail <chmelev.mikhail@gmail.com>
2019-11-30 23:42:48 +0300

    feat: added history and points to Profile page

--- user_profile/routes.py
+++ user_profile/routes.py
@@ -1,6 +1,6 @@
 from flask import Blueprint, redirect, url_for, flash, render_template
 from flask_login import current_user, login_required
-from webapp.models import User, Group
+from webapp.models import User, Group, Deadline_status, Deadline, Level, members
 from webapp import db
 from user_profile.forms import change_pass, change_name, change_mail
 
@@ -12,12 +12,20 @@ def profile():
     change_mail_form = change_mail()
     change_name_form = change_name()
     change_pass_form = change_pass()
-    if change_mail_form.validate_on_submit():
-        print('good')
+    if change_mail_form.validate_on_submit() and change_mail_form.mail.data:
         return 'change_mail done'
-    if change_pass_form.validate_on_submit():
+    if change_pass_form.validate_on_submit() and change_pass_form.pasword.data:
         return 'change_pass done'
-    if change_name_form.validate_on_submit():
+    if change_name_form.validate_on_submit() and change_name_form.name.data:
         return 'change_name done'
+
+    history = Deadline_status.query.filter_by(user_id=current_user.id) \
+        .join(Deadline) \
+        .add_columns(Deadline.title, Deadline_status.status) \
+        .filter(Deadline_status.status < 2) \
+        .join(Level).add_columns(Level.value).all()
+
+    points = db.session.query(members).filter_by(user_id=current_user.id, group_id=current_user.self_group.id).first().points
+
     return render_template('account/profile.html', change_mail_form=change_mail_form, change_pass_form=change_pass_form,
-                           change_name_form=change_name_form)
+                           change_name_form=change_name_form, history=history, points=points)
